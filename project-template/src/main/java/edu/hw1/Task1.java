e58864dea19536b0cc46a531822e8d5a95e1ff81
Jopick <piple05@mail.ru>
2023-10-13 22:44:57 +0500

    resot Task1 and try test writing

--- project-template/src/main/java/edu/hw1/Task1.java
+++ project-template/src/main/java/edu/hw1/Task1.java
@@ -1,1 +1,14 @@
-123
+package edu.hw1;
+public class Task1 {
+    public static int countDigits(int args) {
+        int ch = 0;
+
+        while (args > 9) {
+            ch += 1;
+            args /= 10;
+
+        }
+        ch += 1;
+        return ch;
+    }
+}
---
+++ project-template/src/test/java/edu/hw1/Task1_Test.java
@@ -0,0 +1,22 @@
+package edu.hw1;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static net.bytebuddy.matcher.ElementMatchers.is;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class Task1_Test {
+    @Test
+    @DisplayName("Фильтрация четных чисел")
+    void typo(){
+        int ap = 4;
+        //assertThat(Task1.countDigits(4666), is (4));
+        //assertThat(Task1.countDigits)
+        //    .containsExactly(4666)
+        //    .hasSize(4);
+        //System.out.println(Task1.countDigits(4666));
+        //System.out.println(Task1.countDigits(544));
+        //System.out.println(Task1.countDigits(0));
+
+    }
+}
